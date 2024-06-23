08d87926f367cc75e603bd025eb0c47af65290bc
tlavksdmlskf <127850569+tlavksdmlskf@users.noreply.github.com>
2024-06-23 16:46:29 +0900

    객체 포인터/배열, 동적 메모리, string 클래스

---
+++ i.cpp
@@ -0,0 +1,20 @@
+//#include "iostream"
+//#include "string"
+//using namespace std;
+//
+//
+//int main() {
+//	string str;
+//	string address("辑匡矫 己合备 伙己悼 389");
+//	string copyAddress(address);
+//
+//	char text[] = { 'L','o','v','e',' ','C','+','+','\0' };
+//	string title(text);
+//
+//	cout << str << endl;
+//	cout << address << endl;
+//	cout << copyAddress<< endl;
+//	cout << title<< endl;
+//
+//
+//}
