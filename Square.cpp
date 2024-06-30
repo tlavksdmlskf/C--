f788f6a7bde8607ee0739fe64d49f9f353984bb6
tlavksdmlskf <127850569+tlavksdmlskf@users.noreply.github.com>
2024-06-30 18:58:43 +0900

    오버로딩

---
+++ Square.cpp
@@ -0,0 +1,17 @@
+#include "iostream"
+using namespace std;
+
+float square(float a) {
+	return a * a;
+}
+
+double square(double a) {
+	return a * a;
+}
+
+int main() {
+	cout << square(3.0);
+	//cout << square(3);
+	cout << square(3.0f);
+
+}
